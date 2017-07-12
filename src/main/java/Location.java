e90db4d0f217f0f1b8220ea86e25c9bc8a7db5bb
Joe Kramer <joekramer1125@gmail.com>
2017-07-12 09:19:47 -0700

    all framework with database setup

---
+++ src/main/java/Location.java
@@ -0,0 +1,6 @@
+public class Location {
+  private int id;
+  private String name;
+  
+
+}
---
+++ src/test/java/LocationTest.java
@@ -0,0 +1,10 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.sql2o.*;
+import java.util.Arrays;
+
+public class LocationTest {
+
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+}
