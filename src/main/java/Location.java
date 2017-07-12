40f9ba4091fca8b796b60489d389207bb8a63127
Joe Kramer <joekramer1125@gmail.com>
2017-07-12 10:26:51 -0700

    add methods save, getId, equals, and all. All tests pass

--- src/main/java/Location.java
+++ src/main/java/Location.java
@@ -1,3 +1,7 @@
+import java.util.List;
+import java.util.ArrayList;
+import org.sql2o.*;
+
 public class Location {
   private int id;
   private String name;
@@ -10,4 +14,34 @@ public class Location {
     return name;
   }
 
+  public void save() {
+    String sql = "INSERT INTO locations (name) VALUES (:name);";
+    try(Connection con = DB.sql2o.open()) {
+      this.id = (int) con.createQuery(sql, true)
+      .addParameter("name", this.name)
+      .executeUpdate()
+      .getKey();
+    }
+  }
+
+  public static List<Location> all() {
+    String sql = "SELECT id, name FROM locations;";
+    try(Connection con = DB.sql2o.open()) {
+      return con.createQuery(sql).executeAndFetch(Location.class);
+    }
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  @Override
+  public boolean equals(Object location) {
+    if(!(location instanceof Location)) {
+      return false;
+    } else {
+      Location newLocation = (Location) location;
+      return this.getName().equals(newLocation.getName()) && this.getId() == newLocation.getId();
+    }
+  }
 }
--- src/test/java/LocationTest.java
+++ src/test/java/LocationTest.java
@@ -19,4 +19,35 @@ public class LocationTest {
     Location testLocation = new Location("Ballard");
     assertEquals("Ballard", testLocation.getName());
   }
+
+  @Test
+  public void save_savesIntoDatabase_true() {
+    Location testLocation = new Location("Ballard");
+    testLocation.save();
+    assertTrue(Location.all().get(0).equals(testLocation));
+  }
+
+  @Test
+  public void all_returnsAllLocations_true() {
+    Location testLocation1 = new Location("Ballard");
+    testLocation1.save();
+    Location testLocation2 = new Location("Capital Hill");
+    testLocation2.save();
+    assertTrue(Location.all().get(0).equals(testLocation1));
+    assertTrue(Location.all().get(1).equals(testLocation2));
+  }
+
+  @Test
+  public void equals_returnsTrueIfLocationsEqual_true() {
+    Location testLocation1 = new Location("Ballard");
+    Location testLocation2 = new Location("Ballard");
+    assertTrue(testLocation1.equals(testLocation2));
+  }
+
+  @Test
+  public void getId_returnsId_true() {
+    Location testLocation = new Location("Ballard");
+    testLocation.save();
+    assertTrue(testLocation.getId() > 0);
+  }
 }
